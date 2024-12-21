dbdeae98ce1173d478665d3bc8416d4d51652d0d
MrSpaar <julien.pistre@icloud.com>
2024-12-21 17:30:06 +0100

    Robot data + Icon compilation

--- includes/gui.h
+++ includes/gui.h
@@ -5,28 +5,56 @@
 #include <gtkmm/box.h>
 #include <gtkmm/grid.h>
 #include <gtkmm/entry.h>
+#include <gtkmm/label.h>
+#include <gtkmm/image.h>
 #include <gtkmm/button.h>
 #include <gtkmm/window.h>
+#include <unordered_map>
 
 
-class Gui : public Gtk::Window {
+class ControlGrid: public Gtk::Grid {
+public:
+    void addButton(
+        const char *iconName, int row, int col, std::function<void()> callback
+    );
+};
+
+
+class LabeledIcon: public Gtk::Button {
+public:
+    LabeledIcon(const char *iconName, const char *name, const char *text);
+    void setText(const std::string &text);
+private:
+    Gtk::Box container;
+    Gtk::Image image;
+    Gtk::Label label;
+};
+
+
+class DataGrid: public Gtk::Grid {
+public:
+    void addLabel(
+        const char *name, const char *icon, const char *text, int row, int col
+    );
+
+    void updateLabel(const std::string &name, const std::string &value);
+private:
+    std::unordered_map<std::string, LabeledIcon*> labels;
+};
+
+
+class Gui: public Gtk::Window {
 public:
 	Gui();
 	~Gui();
 private:
 	Wifibot robot;
-	Gtk::Box container;
+
+	Gtk::Grid container;
+	ControlGrid controls;
+	DataGrid data;
 
 	Gtk::Box connectBox;
 	Gtk::Entry ipEntry;
 	Gtk::Button connectButton;
-
-	Gtk::Grid buttonGrid;
-	Gtk::Button upButton;
-	Gtk::Button downButton;
-	Gtk::Button stopButton;
-	Gtk::Button leftButton;
-	Gtk::Button rightButton;
-	Gtk::Button rotLButton;
-	Gtk::Button rotRButton;
 };
