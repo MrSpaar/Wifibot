e9b366f192fcbf508f705abb564044f70c65627b
MrSpaar <julien.pistre@icloud.com>
2024-12-21 17:30:06 +0100

    Robot data + Icon compilation

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 
 int main(int argc, char *argv[]) {
-    auto app = Gtk::Application::create("wifibot.polytech.nancy");
+    auto app = Gtk::Application::create("org.wifibot");
     return app->make_window_and_run<Gui>(argc, argv);
 
 	// Gio::init();
--- src/gui.cpp
+++ src/gui.cpp
@@ -1,82 +1,119 @@
 #include "includes/gui.h"
-#include <iostream>
 
+
+void ControlGrid::addButton(
+    const char *iconName, int row, int col, std::function<void()> callback
+) {
+    auto button = Gtk::make_managed<Gtk::Button>();
+
+    button->signal_clicked().connect(callback);
+    button->set_image_from_icon_name(iconName);
+
+    attach(*button, col, row);
+}
+
+LabeledIcon::LabeledIcon(const char *iconName, const char *name, const char *text) {
+    label.set_text(text);
+    image.set_from_icon_name(iconName);
+
+    container.append(image);
+    container.append(label);
+    container.set_spacing(5);
+    container.set_halign(Gtk::Align::CENTER);
+
+    set_child(container);
+    set_tooltip_text(name);
+    set_sensitive(false);
+}
+
+void LabeledIcon::setText(const std::string &text) {
+    label.set_text(text);
+}
+
+void DataGrid::addLabel(
+    const char *name, const char *iconName, const char *text, int row, int col
+) {
+    labels[name] = Gtk::make_managed<LabeledIcon>(iconName, name, text);
+    attach(*labels[name], col, row);
+}
+
+void DataGrid::updateLabel(const std::string &name, const std::string &value) {
+    labels[name]->setText(value);
+}
 
 Gui::Gui() {
     set_child(container);
     set_title("Wifibot");
 	set_resizable(false);
 
-	connectButton.set_label("Connect");
+	ipEntry.set_hexpand(true);
 	ipEntry.set_margin_end(5);
 	ipEntry.set_text("127.0.0.1");
 
+	connectButton.set_label("Connect");
+	connectButton.signal_clicked().connect([&] {
+		robot.connect(ipEntry.get_text());
+	});
+
 	connectBox.append(ipEntry);
 	connectBox.append(connectButton);
-	connectBox.set_margin(5);
-	connectBox.set_orientation(Gtk::Orientation::HORIZONTAL);
 
-	upButton.set_image_from_icon_name("go-up-symbolic");
-	buttonGrid.attach(upButton, 1, 0);
-
-	downButton.set_image_from_icon_name("go-down-symbolic");
-	buttonGrid.attach(downButton, 1, 2);
-
-	leftButton.set_image_from_icon_name("go-previous-symbolic");
-	buttonGrid.attach(leftButton, 0, 1);
-
-	rightButton.set_image_from_icon_name("go-next-symbolic");
-	buttonGrid.attach(rightButton, 2, 1);
-
-	rotLButton.set_image_from_icon_name("object-rotate-left-symbolic");
-	buttonGrid.attach(rotLButton, 0, 2);
-
-	rotRButton.set_image_from_icon_name("object-rotate-right-symbolic");
-	buttonGrid.attach(rotRButton, 2, 2);
-
-	stopButton.set_image_from_icon_name("media-playback-stop-symbolic");
-	buttonGrid.attach(stopButton, 1, 1);
-
-	buttonGrid.set_margin_bottom(5);
-	buttonGrid.set_halign(Gtk::Align::CENTER);
-
-	container.append(connectBox);
-	container.append(buttonGrid);
-	container.set_halign(Gtk::Align::CENTER);
-	container.set_orientation(Gtk::Orientation::VERTICAL);
-
-	connectButton.signal_clicked().connect([this]() {
-		robot.connect(ipEntry.get_text());
-		std::cout << "Connexion Wifibot" << std::endl;
+	controls.addButton("up-button-symbolic", 0, 1, [&] {
+	   robot.speedUp();
 	});
 
-	upButton.signal_clicked().connect([this]() {
-		robot.speedUp();
+	controls.addButton("down-button-symbolic", 2, 1, [&] {
+	   robot.speedDown();
 	});
 
-	downButton.signal_clicked().connect([this]() {
-		robot.speedDown();
+	controls.addButton("left-button-symbolic", 1, 0, [&] {
+	   robot.turn(-1);
 	});
 
-	leftButton.signal_clicked().connect([this]() {
-		robot.turn(-1);
+	controls.addButton("right-button-symbolic", 1, 2, [&] {
+	   robot.turn(+1);
 	});
 
-	rightButton.signal_clicked().connect([this]() {
-		robot.turn(+1);
-	});
+    controls.addButton("rotate-left-button-symbolic", 2, 0, [&] {
+        robot.rotate(-1);
+    });
 
-	rotLButton.signal_clicked().connect([this]() {
-		robot.rotate(-1);
-	});
+    controls.addButton("rotate-right-button-symbolic", 2, 2, [&] {
+        robot.rotate(+1);
+    });
 
-	rotRButton.signal_clicked().connect([this]() {
-		robot.rotate(+1);
-	});
+    controls.addButton("stop-button-symbolic", 1, 1, [&] {
+        robot.stop();
+    });
 
-	stopButton.signal_clicked().connect([this]() {
-		robot.stop();
-	});
+	controls.set_margin_end(5);
+	controls.set_halign(Gtk::Align::CENTER);
+	controls.set_valign(Gtk::Align::CENTER);
+
+	data.addLabel("Battery", "battery-label-symbolic", "?%", 0, 0);
+	data.addLabel("Version", "version-label-symbolic", "?", 0, 1);
+	data.addLabel("Current", "current-label-symbolic", "?A", 0, 2);
+
+	data.addLabel("Left Speed", "speedometer-label-symbolic", "?", 1, 0);
+	data.addLabel("Left IR1", "ir-label-symbolic", "?", 2, 0);
+	data.addLabel("Left IR2", "ir-label-symbolic", "?", 3, 0);
+	data.addLabel("Left Odometry", "odometry-label-symbolic", "?", 4, 0);
+
+	data.addLabel("Right Speed", "speedometer-label-symbolic", "?", 1, 2);
+	data.addLabel("Right IR1", "ir-label-symbolic", "?", 2, 2);
+	data.addLabel("Right IR2", "ir-label-symbolic", "?", 3, 2);
+	data.addLabel("Right Odometry", "odometry-label-symbolic", "?", 4, 2);
+
+	data.set_row_spacing(5);
+	data.set_column_spacing(5);
+	data.set_halign(Gtk::Align::CENTER);
+
+	container.attach(connectBox, 0, 0, 2, 1);
+	container.attach(controls, 0, 1);
+	container.attach(data, 1, 1);
+	container.set_margin(10);
+	container.set_row_spacing(10);
+	container.set_column_spacing(10);
 }
 
 Gui::~Gui() {
