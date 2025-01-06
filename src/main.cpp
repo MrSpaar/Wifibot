40c112a133f2ee3b5a2ac4929bcacbdcc28a9da3
MrSpaar <julien.pistre@icloud.com>
2025-01-06 21:01:00 +0100

    Renamed app

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,6 @@
 
 
 int main(int argc, char *argv[]) {
-    auto app = Gtk::Application::create("org.wifibot");
+    auto app = Gtk::Application::create("com.github.mrspaar.Wifibot");
     return app->make_window_and_run<Gui>(argc, argv);
 }
